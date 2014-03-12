32171ab26d301bb5ea1fd56fb7fa017eab83f778
dcfrederique <dcfrederique@gmail.com>
2014-03-12 08:52:48 +0100

    ShapeGame

---
+++ Assets/Scripts/AnswerScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnswerScript : MonoBehaviour
+{
+
+		// Use this for initialization
+		public string correctAnswer;
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		void OnTriggerEnter (Collider colliderinfo)
+		{
+				if (correctAnswer.Equals (colliderinfo.name)) {
+						Destroy (colliderinfo.gameObject.GetComponent ("tk2dUIDragItem"));
+						Destroy (colliderinfo);
+						Destroy (gameObject.collider);
+						float x = gameObject.transform.position.x;
+						float y = gameObject.transform.position.y;
+						colliderinfo.gameObject.transform.position = new Vector3 (x, y, colliderinfo.gameObject.transform.position.z);
+				}
+		}
+}
